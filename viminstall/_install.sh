78ba81c43bcb00f52ddd475fdcad8f7db7d1a274
Doug Ilijev <doug.ilijev@gmail.com>
2015-05-03 23:27:25 -0500

    personal branch of monokai with installer, script to install all vim plugins

---
+++ viminstall/_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# install all vim plugins using the defined scripts
+./ctrlp.sh
+./paredit.sh
+./rainbow.sh
+./solarized.sh
+./monokai.sh
---
+++ viminstall/monokai.sh
@@ -0,0 +1,3 @@
+mkdir ~/.vim/colors
+wget -O ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/dilijev/vim-monokai/master/colors/monokai.vim
+wget -O ~/.vim/colors/monokai-lisp.vim https://raw.githubusercontent.com/dilijev/vim-monokai/master/colors/monokai-lisp.vim
