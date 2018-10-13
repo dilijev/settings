faf0239b45f4e25622addd53f3e6e507e4d9d37d
Doug Ilijev <doilij@microsoft.com>
2018-10-12 20:08:09 -0700

    [wip] update rainbow.vim install script

--- viminstall/rainbow.sh
+++ viminstall/rainbow.sh
@@ -1,2 +1,7 @@
+# git clone https://github.com/luochen1990/rainbow.git
+# cd rainbow
+# cp plugin/* ~/.vim/plugin
+# cp autoload/* ~/.vim/autoload
+
 mkdir ~/.vim/plugin
 wget -O ~/.vim/plugin/rainbow.vim https://raw.githubusercontent.com/luochen1990/rainbow/master/plugin/rainbow.vim
